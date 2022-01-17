0a8b78ab2f6964c84397bedb4188171422c2e44b
djohnstone008 <95123827+djohnstone008@users.noreply.github.com>
2022-01-18 00:51:42 +0800

    image gallery

--- _site/scripts.js
+++ _site/scripts.js
@@ -14,3 +14,16 @@ $( document ).ready(function() {
   });
 
 });
+
+// image gallery with lightgallery
+lightGallery(document.getElementById('lightgallery'), {
+  speed: 500,
+  download: false
+  
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+  speed: 500,
+  download: false
+  
+});
